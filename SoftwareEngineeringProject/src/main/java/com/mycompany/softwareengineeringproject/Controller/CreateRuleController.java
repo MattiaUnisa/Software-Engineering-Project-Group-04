6db8dcdde43755d002b46aa63679ed2964861611
Lenovo <Lenovo@LAPTOP-DQCMLOCK.Home>
2025-12-06 17:42:36 +0100

    Merge origin/master
    Conflicts:
    	SoftwareEngineeringProject/src/main/resources/com/mycompany/softwareengineeringproject/createRule.fxml
    	SoftwareEngineeringProject/src/main/resources/com/mycompany/softwareengineeringproject/home.fxml

--- SoftwareEngineeringProject/src/main/java/com/mycompany/softwareengineeringproject/Controller/CreateRuleController.java
+++ SoftwareEngineeringProject/src/main/java/com/mycompany/softwareengineeringproject/Controller/CreateRuleController.java
@@ -1,26 +1,26 @@
 package com.mycompany.softwareengineeringproject.Controller;
 
 import com.mycompany.softwareengineeringproject.App;
+import com.mycompany.softwareengineeringproject.Model.Rule;
+import com.mycompany.softwareengineeringproject.Model.RuleEngine;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
 public class CreateRuleController {
-
+    
+    // takes the name from the tag in fxml
     @FXML 
     private TextField nameField;
 
-    /**
-     * Gestisce il click sulla freccia "Indietro".
-     * Torna alla schermata Home senza salvare nulla.
-     */
+     // Go back to the home screen saving nothing
     @FXML
     private void onBackClick() throws IOException {
         App.setRoot("home");
     }
 
-
+    // go back to the home schreen saving the rule
     @FXML
     private void onSaveClick() throws IOException {
         String name = nameField.getText();
@@ -32,9 +32,18 @@ public class CreateRuleController {
             alert.setHeaderText("Name missing");
             alert.setContentText("Please insert a name for the rule.");
             alert.showAndWait();
-            return; // Interrompe l'esecuzione se il nome manca
+            return; 
         }
+        
+        // empty rule 
+        Rule newRule = new Rule(name, null, null);
 
+        // add the rule to the singleton 
+        RuleEngine.getInstance().addRule(newRule);
+        
+        // debug in console
+        System.out.println("Rule Saved: " + name); 
+        
         // TO DO
         
         App.setRoot("home");
