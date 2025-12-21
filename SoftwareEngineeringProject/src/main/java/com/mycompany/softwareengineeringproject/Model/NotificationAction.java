7f46ab3812fa1710eec65e534a951dc83631a928
Lenovo <Lenovo@LAPTOP-DQCMLOCK.Home>
2025-12-21 15:43:57 +0100

    Merge origin/master
    Conflicts:
    	SoftwareEngineeringProject/RulesFile.txt
    	SoftwareEngineeringProject/src/main/resources/MoveFileAction.fxml
    	SoftwareEngineeringProject/src/test/java/com/mycompany/softwareengineeringproject/Test/MoveFileActionTest.java

--- SoftwareEngineeringProject/src/main/java/com/mycompany/softwareengineeringproject/Model/NotificationAction.java
+++ SoftwareEngineeringProject/src/main/java/com/mycompany/softwareengineeringproject/Model/NotificationAction.java
@@ -1,32 +1,22 @@
 package com.mycompany.softwareengineeringproject.Model;
 
-/**
- * A simple action that shows a notification message when a rule is triggered.
- */
+// A simple action that shows a notification message when a rule is triggered.
 public class NotificationAction implements Action {
 
     // The text that will be shown to the user
     private final String message;
 
-    /**
-     * Constructor: Stores the message written by the user.
-     */
+    //Stores the message written by the user.
     public NotificationAction(String message) {
         this.message = message;
     }
 
-    /**
-     * Returns the message (useful for the UI or logs).
-     */
+    //Returns the message (useful for the UI or logs).
     public String getMessage() {
         return message;
     }
 
-    /**
-     * When the rule is triggered, the RuleEngine calls execute().
-     * Here is where we show the notification.
-     * For now, we just print it. Later you can show it with JavaFX.
-     */
+    //When the rule is triggered, the RuleEngine calls execute().
     @Override
     public void execute(ActionContext context) {
         String title = "Notification";
@@ -41,9 +31,7 @@ public class NotificationAction implements Action {
         stop();
     }
 
-    /**
-     * This action has nothing to stop, so this method stays empty.
-     */
+    //This action has nothing to stop, so this method stays empty.
     @Override
     public void stop() {
         // No stop behavior needed for a simple notification
